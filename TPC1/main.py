cf14bcd0f4e97b9cbed5fe700b64b24a8554b71b
Artur Leite <arturleite164@gmail.com>
2023-02-19 11:04:04 +0000

    changed the values to rates

--- TPC1/main.py
+++ TPC1/main.py
@@ -33,48 +33,58 @@ def read_file(file):
     return data, greater_age, greater_colesterol, minor_colesterol
             
 def rate_doenca_sexo(data):
-    total = len(data)
+    data = list(filter(lambda x: x.temDoenca, data))
+    totalDoenca = len(data)
     masc, fem = 0, 0
-    masc, fem = list(filter(lambda x: x.sexo == 'M' and x.temDoenca, data)), list(filter(lambda y: y.sexo == 'F' and y.temDoenca, data))
-    return {"Masculino": len(masc)/total, "Feminino": len(fem)/total}
+    masc, fem = list(filter(lambda x: x.sexo == 'M', data)), list(filter(lambda y: y.sexo == 'F', data))
+    masc = len(masc)
+    fem = len(fem)
+    return {"Masculino": (float(masc)/totalDoenca), "Feminino": float(fem)/totalDoenca}
 
 def rate_doenca_idade(data, limite_idade):
+    data = list(filter(lambda x: x.temDoenca, data))
+    total_doenca = len(data)
     min_age = 30
     max_age = limite_idade
-    age_ranges = {}
     age_array_size = 5
-    for age in range(min_age, max_age + 1, age_array_size):
-        age_ranges[f"[{age}-{age + age_array_size - 1}]"] = 0
+    age_ranges = {f"[{age}-{age + age_array_size - 1}]": 0.0 
+                  for age in range(min_age, max_age + 1, age_array_size)}
 
     for person in data:
         for age_range in age_ranges.keys():
             start, end = map(int, age_range[1:-1].split('-'))
-            if person.temDoenca and person.idade >= start and person.idade <= end:
+            if person.idade >= start and person.idade <= end:
                 age_ranges[age_range] += 1
                 break
 
+    for age_range in age_ranges.keys():
+        age_ranges[age_range] /= total_doenca
     return age_ranges
 
 def rate_doenca_colesterol(data, limite_sup_colesterol, limite_inf_colesterol):
+    data = list(filter(lambda x: x.temDoenca, data))
+    totalDoenca = len(data)
     min = limite_inf_colesterol
     max = limite_sup_colesterol
     array_range_size = 11
-    colesterol_ranges = {}
-    for colesterol in range(min, max + 1, array_range_size):
-        colesterol_ranges[f"[{colesterol}-{colesterol + array_range_size - 1}]"] = 0
+    colesterol_ranges = {f"[{colesterol}-{colesterol + array_range_size - 1}]": 0.0 
+                         for colesterol in range(min, max + 1, array_range_size)}
 
     for person in data:
         for colesterol_range in colesterol_ranges.keys():
             start, end = map(int, colesterol_range[1:-1].split('-'))
-            if person.temDoenca and person.colesterol >= start and person.colesterol <= end:
+            if person.colesterol >= start and person.colesterol <= end:
                 colesterol_ranges[colesterol_range] += 1
                 break
+
+    for colesterol_range in colesterol_ranges.keys():
+        colesterol_ranges[colesterol_range] /= totalDoenca
     return colesterol_ranges
 
 def get_tabela(distribuicao):
     res = ""
     for key, value in distribuicao.items():
-        res += f"{key: ^20} | {value: ^20}\n"
+        res += f"{key: ^20} | {(value * 100): ^20}\n"
     return res
 
 def main():
